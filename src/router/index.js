7f426649cdd7768143ffe52796b0dda46b8a19c5
Hadeel-Ayman <haddelayman4@gmail.com>
2023-02-12 23:44:21 +0300

    add components and pages

---
+++ src/router/index.js
@@ -0,0 +1,7 @@
+import About from "../pages/about";
+import Home from "../pages/Home";
+
+export const router = [
+    { path: '/', element: <Home /> },
+    { path: '/about', element: <About /> },
+]
